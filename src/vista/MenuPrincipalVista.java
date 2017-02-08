da6f1625633e7b7eb69260fd69da5c6a85530556
oscar <oscergar@gmail.com>
2017-02-08 17:42:25 +0100

    yokse, no he tocao nah

--- src/vista/MenuPrincipalVista.java
+++ src/vista/MenuPrincipalVista.java
@@ -57,7 +57,6 @@ public class MenuPrincipalVista extends JFrame  {
         MenuPrincipal.add(menuButtons[1]);
         MenuPrincipal.add(menuButtons[2]);
         MenuPrincipal.add(menuButtons[3]);
-        //caracol
         
          
         add(MenuPrincipal);
